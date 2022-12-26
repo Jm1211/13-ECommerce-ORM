03df9a7e5907e358549b383844f1548f0854620f
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-12-26 16:18:53 -0700

    add models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
 // Products belongsTo Category
+Product.belongsTo(Category); 
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+User.belongsToMany(Post,{
+  through: ProductTag,
+  as: 'ProductTag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, {
+  through: ProductTag,
+  as: 'ProductTag'
+});
+
 
 module.exports = {
   Product,
