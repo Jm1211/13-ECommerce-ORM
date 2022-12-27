ddc0abff245c982c223082c27e93d2ddb2048dbf
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-12-26 21:44:41 -0700

    Get all products working, get one and post new category not working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { rest } = require('lodash');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -21,11 +20,39 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No Category found with this ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+    })
+      .then(dbCategoryData => res.json(dbCategoryData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 router.put('/:id', (req, res) => {
